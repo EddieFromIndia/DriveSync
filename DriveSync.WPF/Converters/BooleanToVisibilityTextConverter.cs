c7627093bfbaab27a17ebb14a320a334f371342e
EddieFromIndia <bassistatanu@gmail.com>
2021-10-17 12:19:46 +0530

    Adjusting new menu

---
+++ DriveSync.WPF/Converters/BooleanToVisibilityTextConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace DriveSync
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    public class BooleanToVisibilityTextConverter : IValueConverter
+    {
+        public static BooleanToVisibilityTextConverter Instance = new BooleanToVisibilityTextConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "Hide equal files/folders" : "Show equal files/folders";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
