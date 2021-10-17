1eaaae19960db81a0948f92598c1ab778c05a33d
EddieFromIndia <bassistatanu@gmail.com>
2021-10-17 16:12:56 +0530

    Adjusted visibility of address bar

---
+++ DriveSync.WPF/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DriveSync
+{
+    [ValueConversion(typeof(string), typeof(Visibility))]
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public static StringToVisibilityConverter Instance = new StringToVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty(value.ToString()) ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
